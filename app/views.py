e91058950f448b2d8c37e90bfb7ac94847c43685
AashishAdhikari27 <aashishadhikari2027@gmail.com>
2024-11-23 22:28:29 +0545

    project initialized

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,  redirect, get_object_or_404
+from django.conf import settings
 
-# Create your views here.
+
+
+
+def home(request):
+
+    # Define your message
+    message = "Welcome to our Plant Leaf Diseases Classifier System !!"
+    
+    # Pass the message to the template context
+    return render(request, 'app/home.html', {'message': message})
+
