64802e2d9c57e07a1547eba48a52470163262cae
AashishAdhikari27 <aashishadhikari2027@gmail.com>
2024-11-26 22:13:39 +0545

    updated

--- plant_leaf/settings.py
+++ plant_leaf/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'plant_leaf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'plant_db',                     # database name
+        'USER': 'postgres',                     # PostgreSQL user
+        'PASSWORD': 'postgres',                 # Password for 'postgres' user
+        'HOST': 'localhost',                    # Host (use 'localhost' for local setup)
+        'PORT': '5432',                         # Default PostgreSQL port
     }
 }
 
